1d2827ef7487d4055b839786cd00c0f1a2eb30ef
kikiraihan <mohzulkiflikatili@gmail.com>
2023-06-04 14:07:35 +0700

    bentuk tampilan

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<template style="background: #edf2f7;">
+    <main class="min-h-screen w-full bg-gray-100 text-gray-700" x-data="layout">
+        <div class="flex h-screen">
+            <Sidebar/>
+            <div class="w-full md:flex-auto overflow-scroll scroll-m-2">
+                <router-view/>
+            </div>
+        </div>
+    </main>
+
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+  
 </template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar
+  }
+}
+</script>
+
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  /* -webkit-font-smoothing: antialiased; */
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
   color: #2c3e50;
 }
 
