269d41fb3c67e65376c98a446fd7cad2cbb0a6e2
kikiraihan <mohzulkiflikatili@gmail.com>
2023-10-28 20:49:21 +0700

    fix collapse a href

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,18 @@
 <template>
     <nav
         class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between  px-6 bg-white shadow sm:items-baseline w-full">
-        <a class="flex items-center py-3" href="/">
+        <div class="flex items-center py-3">
             <!-- <img class="w-auto h-8 inline" src="@/assets/logo-2.png" alt=""/> -->
-            <i class="bx bxs-bug text-2xl text-orange-300 rotate-12"></i>
-            <span class="font-bold text-2xl" style="font-family: 'Oswald', sans-serif;"><span class="text-orange-300">Vektor</span>pedia</span>
+            <a href="/">
+                <i class="bx bxs-bug text-2xl text-orange-300 rotate-12"></i>
+                <span class="font-bold text-2xl" style="font-family: 'Oswald', sans-serif;"><span class="text-orange-300">Vektor</span>pedia</span>
+            </a>
             <select v-model="$i18n.locale" class="cursor-pointer border border-1 border-gray-500 rounded ml-3 opacity-50" @change="changeLocale($event.target.value)">
                 <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
                     {{ locale }}
                 </option>
             </select>
-        </a>
+        </div>
         <div class="flex flex-row">
             <router-link v-for="route in routes" :key="route.path" :to="route.path"
               class="px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 flex flex-row items-center">
